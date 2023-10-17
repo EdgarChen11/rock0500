17bcdd391151a5b57ee842428b4adb49df40a2dd
EdgarChen11 <110076175+EdgarChen11@users.noreply.github.com>
2023-10-17 14:21:52 +0800

    增加 Unicode 轉換字符

--- 呼叫API模板.py
+++ 呼叫API模板.py
@@ -3,22 +3,22 @@ import json
 
 url = "https://api.bitopro.com/v3/tickers/usdt_twd"
 
-response = requests.get(url)
-
-
 headers = {
     "Authorization": "Bearer token",
 }
 
-body = {
+data = {
     "key": "value", 
 }
 
 
 
+response = requests.post(url, data=data, headers=headers)
+
 if response.status_code == 200:
     data = response.json()
     formatted_data = json.dumps(data, indent=4, sort_keys=True)
-    print(formatted_data)
+    message = formatted_data.encode("UTF-8").decode('unicode_escape')
+    print(message)
 else:
     print(f"無法執行，狀態碼:, {response.status_code}")
